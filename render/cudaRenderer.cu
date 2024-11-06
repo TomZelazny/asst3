5acace859f4956240489aa9ba6793848070f70a1
TomZelazny <Tom.Zelazny@mail.huji.ac.il>
2024-11-06 05:05:42 -0800

    refactor kernelRenderPixels to restore circle boundary check for accurate rendering

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -465,15 +465,13 @@ __global__ void kernelRenderPixels() {
         int index3 = 3 * i;
         float3 p = *(float3*)(&cuConstRendererParams.position[index3]);
         float rad = cuConstRendererParams.radius[i];
-        
-        int cpas = circleInBoxConservative(p.x, p.y, rad, left, right, top, bottom);
-        
-        // if(circleInBoxConservative(p.x, p.y, rad, left, right, top, bottom) == 0) {
-        //     sharedPositions[relevant_circles_count] = p;
+                
+        if(circleInBoxConservative(p.x, p.y, rad, left, right, top, bottom) == 0) {
+            sharedPositions[relevant_circles_count] = p;
         //     sharedRadii[relevant_circles_count] = rad;
         //     sharedColors[relevant_circles_count] = *(float3*)(&cuConstRendererParams.color[index3]);
         //     relevant_circles_count++;
-        // }
+        }
 
     }
     __syncthreads();
